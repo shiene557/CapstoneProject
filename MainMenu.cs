05813a7ff6460eef74c61f106be7bf7ad24c4ae2
shiene557 <shiene557@gmail.com>
2018-12-06 18:01:03 +0800

    new

---
+++ MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+   
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void TakePhoto()
+    {
+        SceneManager.LoadScene("Camera");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ViewCapturedPhoto()
+    {
+        SceneManager.LoadScene("ImageViewer");
+    }
+
+    public void ExitApp()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
